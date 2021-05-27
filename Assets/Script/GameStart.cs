015e6e6e7e65ecbd854484f58c82bd21746a2742
RYOMA1205 <ryoma.take@gmail.com>
2021-05-27 22:38:48 +0900

    コミット 手順22まで完了　基礎編完成

---
+++ Assets/Script/GameStart.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    // 22で作成
+    // 先頭に「public」をつけること(ポイント)
+    public void OnGAMESTARTButtonClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
