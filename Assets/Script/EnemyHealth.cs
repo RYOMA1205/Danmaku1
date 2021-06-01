1ae5ec9f85b4bb99e60b8bc17375faf2ab5da42f
RYOMA1205 <ryoma.take@gmail.com>
2021-06-01 18:32:18 +0900

    コミット2　手順7の途中まで

--- Assets/Script/EnemyHealth.cs
+++ Assets/Script/EnemyHealth.cs
@@ -12,8 +12,12 @@ public class EnemyHealth : MonoBehaviour
 
     public int enemyHP;
 
+    public int enemyHPcanvas;
+
     // 10で追加
     private Slider slider;
+
+    private Slider slidercanvas;
 
     // 19で追加(スコア)
     public int scoreValue;
@@ -26,11 +30,17 @@ public class EnemyHealth : MonoBehaviour
         // (ポイント)GameObject.Find("○○")の使い方を覚えよう。名前でオブジェクトを指定できる
         slider = GameObject.Find("EnemyHPSlider").GetComponent<Slider>();
 
+        slidercanvas = GameObject.Find("EnemyCanvas").GetComponent<Slider>();
+
         // スライダーの最大値の設定
         slider.maxValue = enemyHP;
 
         // スライダーの現在地の設定
         slider.value = enemyHP;
+
+        slidercanvas.maxValue = enemyHPcanvas;
+
+        slidercanvas.value = enemyHPcanvas;
 
         // 19で追加(スコア)
         // 「ScoreLabel」オブジェクトについている
@@ -52,15 +62,33 @@ public class EnemyHealth : MonoBehaviour
             // 敵のHPを1つずつ減少させる
             enemyHP -= 1;
 
+            enemyHPcanvas -= 1;
+
             // 10で追加
             // この一行を追加しないとスライダーバーの目盛りが変化しない
             slider.value = enemyHP;
+
+            slidercanvas.value = enemyHPcanvas;
 
             // ミサイルを削除する
             Destroy(other.gameObject);
 
             // 敵のHPが0になったら敵オブジェクトを破壊する
             if (enemyHP == 0)
+            {
+                // 親オブジェクトを破壊する(ポイント;この使い方を覚えよう!)
+                Destroy(transform.root.gameObject);
+
+                // 破壊の効果音を出す
+                AudioSource.PlayClipAtPoint(destroySound, transform.position);
+
+                // 19で追加(スコア)
+                // 敵を破壊した瞬間にスコアを加算するメソッドを呼び出す
+                // 引数には「scoreValue」を入れる
+                sm.AddScore(scoreValue);
+            }
+
+            if (enemyHPcanvas == 0)
             {
                 // 親オブジェクトを破壊する(ポイント;この使い方を覚えよう!)
                 Destroy(transform.root.gameObject);
