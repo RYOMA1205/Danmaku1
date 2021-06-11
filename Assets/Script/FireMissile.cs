d860bc1011cf2f071af41dcec147f375e7e7306c
RYOMA1205 <ryoma.take@gmail.com>
2021-06-12 01:03:20 +0900

    コミット3　手順6まで完了

--- Assets/Script/FireMissile.cs
+++ Assets/Script/FireMissile.cs
@@ -23,6 +23,13 @@ public class FireMissile : MonoBehaviour
     // 5で追加(パワー量の表示)
     private Slider powerSlider;
 
+    // 6で追加(発射パワーの回復)
+    // 発射パワーが回復するまでに要する時間(定数)
+    const int RecoveryTime = 10;
+
+    // 発射パワー回復までの残り時間
+    private int counter;
+
     // ４で追加(弾切れ発生)
     private void Start()
     {
@@ -38,6 +45,13 @@ public class FireMissile : MonoBehaviour
 
     void Update()
     {
+        // 6で追加(発射パワーの回復)
+        if (shotPower <= 0 && counter <= 0)
+        {
+            // 条件の内容をおさえる(ポイント)
+            // コルーチンを作動させる
+            StartCoroutine(RecoverPower());
+        }
         // 1で追加(長押し連射)
         timeCount += 1;
 
@@ -77,5 +91,31 @@ public class FireMissile : MonoBehaviour
                     Destroy(missile, 2.0f);
                 }
             }
+    }
+
+    // 6で追加(発射パワーの回復)
+    // コルーチン
+    IEnumerator RecoverPower()
+    {
+        // パワー回復までに必要な時間をセット
+        counter = RecoveryTime;
+
+        // 1秒ずつカウントを進める
+        while (counter > 0)
+        {
+            yield return new WaitForSeconds(1.0f);
+
+            counter -= 1;
+
+            print("全回復までの残り時間" + counter + "秒");
+        }
+
+        // (ポイント)
+        // while内の処理が行われている間は、ここのコードは実行されない
+        // whileの処理が終了 = 残り時間が0になった時にコードが実行される
+        // 発射パワーをマックス状態にする
+        shotPower = maxPower;
+
+        powerSlider.value = shotPower;
     }
 }
