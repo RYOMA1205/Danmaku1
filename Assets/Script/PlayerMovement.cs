2bbb104d6e464b14048dd885a5c8cec25d5d12dc
RYOMA1205 <ryoma.take@gmail.com>
2021-06-18 22:08:21 +0900

    コミット4　手順9まで完了

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -9,22 +9,43 @@ public class PlayerMovement : MonoBehaviour
     // 3個目で追加
     private Vector3 pos;
 
+    // 4章の9で修正
     void Update()
     {
-        // 9で改良(下記の2行をコメントアウトする)
-        //float moveH = Input.GetAxis("Horizontal") * moveSpeed;
+        // PCでも動きを確認できる様にします(テクニック)
+        if (Application.isEditor)
+        {
+            // 9で改良(下記の2行をコメントアウトする)
+            // 4章の9でコメントアウト解除
+            float moveH = Input.GetAxis("Horizontal") * moveSpeed;
 
-        //float moveV = Input.GetAxis("Vertical") * moveSpeed;
+            float moveV = Input.GetAxis("Vertical") * moveSpeed;
 
-        // 9で改良(下記の2行を追加する)
-        float moveH = -Input.GetAxis("Mouse X") * moveSpeed;
+            // 9で改良(下記の2行を追加する)
+            // 4章の9でコメントアウト
+            // float moveH = -Input.GetAxis("Mouse X") * moveSpeed;
+            // float moveV = -Input.GetAxis("Mouse Y") * moveSpeed;
 
-        float moveV = -Input.GetAxis("Mouse Y") * moveSpeed;
+            transform.Translate(moveH, 0, moveV);
 
-        transform.Translate(moveH, 0, moveV);
+            // 3個目で追加
+            MoveClamp();
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
 
-        // 3個目で追加
-        MoveClamp();
+                float MoveH = -touch.deltaPosition.x * Time.deltaTime * moveSpeed;
+
+                float MoveV = -touch.deltaPosition.y * Time.deltaTime * moveSpeed;
+
+                transform.Translate(MoveH, 0, MoveV);
+
+                MoveClamp();
+            }
+        }
     }
 
     // 3個目で追加
@@ -32,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
     {
         pos = transform.position;
 
-        pos.x = Mathf.Clamp(pos.x, -10, 10);
+        pos.x = Mathf.Clamp(pos.x, -20, 20);
 
         pos.z = Mathf.Clamp(pos.z, -10, 10);
 
